5773f71ccda992cf167563361e3014393a30d18d
kibuikaCodes <kibuikasteve@gmail.com>
2022-06-27 17:25:14 +0300

    get channel title from HTML element

--- js/content.js
+++ js/content.js
@@ -11,7 +11,7 @@ var observer = new MutationObserver(function (mutations) {
   });
 });
 
-async function checkNode() {
+const checkNode = async () => {
   // get the node that has the youtube video title
   var targetNode = document.querySelector(
     "h1.title.style-scope.ytd-video-primary-info-renderer"
@@ -20,32 +20,32 @@ async function checkNode() {
     window.setTimeout(checkNode, 500); // delay the execution so all content loads.
     return;
   }
-  // fetch request to get video comments
-  let title = await getNewTitleFromComment(videoID); // get the title from the comments (Takes the comment from Steve Kibuika and sets that as title)
-  if(title){
-    // if title is not empty, set the title to the new title
+  // get channel name from HTML elements
+  const channelTitle = document
+    .querySelector("yt-formatted-string.style-scope.ytd-channel-name")
+    .getElementsByTagName("a")[0].innerHTML;
+  if (channelTitle === "The Good Company KE") {
+    // fetch request to get video comments
+    let title = await getNewTitleFromComment(videoID); // get the title from the comments (Takes the comment from Steve Kibuika and sets that as title)
+    console.log(title);
+    // set the title to the new title
     targetNode.innerText = title;
-  } else {
-    targetNode.innerText = "Comment below in order to change title"; // replace the title with the new title ( from Steve's comment )
+    var config = {
+      childList: true,
+      subtree: true,
+    };
+    observer.observe(targetNode, config);
   }
-  var config = {
-    childList: true,
-    subtree: true,
-  };
-  observer.observe(targetNode, config);
-}
-
-let channelName = params.get("ab_channel");
-// TODO: get channel name from html elements instead of url
-if (channelName === "TheGoodCompanyKE") {
-  checkNode();
-}
+};
+checkNode();
 
 const getNewTitleFromComment = async (url) => {
   let title;
-  try{
+  try {
     async function getData() {
-      const response = await fetch(`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=${url}&key=${process.env.YOUTUBE_API_KEY}`);
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId=${url}&key=YOUR_API_KEY`
+      );
       if (!response.ok) {
         const message = `An error has occured: ${response.status}`;
         throw new Error(message);
@@ -54,16 +54,16 @@ const getNewTitleFromComment = async (url) => {
       return comments.items;
     }
     await getData().then((comments) => {
-      console.log(comments);
-    const myComment = comments.filter(comment => {
-      return comment?.snippet?.topLevelComment?.snippet?.authorDisplayName === "Steve Kibuika"
-    })
-    console.log(myComment)
-    title = myComment[0]?.snippet?.topLevelComment?.snippet?.textDisplay;
-    return title;
-  });
-  }catch(e){
-    console.log(e)
+      const myComment = comments.filter((comment) => {
+        return (
+          comment?.snippet?.topLevelComment?.snippet?.authorDisplayName === "Steve Kibuika"
+        );
+      });
+      title = myComment[0]?.snippet?.topLevelComment?.snippet?.textDisplay || "Comment below to change the title";
+      return title;
+    });
+  } catch (e) {
+    console.log(e);
   }
   return title;
 };
